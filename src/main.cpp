0d4761beb30ea55a2e81a1e10220557dc54774cc
codingfemboy <>
2024-05-09 21:55:35 +1200

    add thing

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,15 @@ using namespace std;
 
 int main() {
     string input;
-    bool debug_mode;
     while (getline (cin, input)) {
         if (input == "uci") {
-            cout << "id name MyCoolEngine 1.0\n";
-            cout << "id author codingfemboy\n";
+            cout << "id name MyCoolEngine 1.0\nid author codingfemboy\n\n";
             cout << "option UCI_ELO 800\n";
             cout << "uciok\n";
-        } else if (input == "debug on") {
-            debug_mode = true;
-        } else if (input == "debug off") {
-            debug_mode = false;
-        } else {
+        }
+        else if (input == "debug on") {} else if (input == "debug off") {}
+        else if (input == "isready") cout << "readyok\n";
+        else {
             cout << "Invalid input\n";
         }
     }
